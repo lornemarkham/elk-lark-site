df6226a8b2c8779292e3c928ff2801f152fc7c67
lorne markham <lornemarkham@lornes-MacBook-Pro.local>
2025-06-16 10:13:12 -0700

    test

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,10 @@ import Header from "./components/Header";
 import Home from "./pages/Home";
 import About from "./pages/about2";
 import Services from "./pages/services2";
-import Property from "./pages/Property";
+import Property from "./pages/property2";
 import Gallery from "./pages/Gallery";
 import FAQ from "./pages/Faq";
-import Contact from "./pages/Contact";
+import Contact from "./pages/contact2";
 import Book from "./pages/Book";
 
 // Optional sub-pages if you're using them
@@ -30,7 +30,7 @@ export default function App() {
           <Route path="/property" element={<Property />} />       {/* Basecamp */}
           <Route path="/gallery" element={<Gallery />} />         {/* The Lark Life */}
           <Route path="/faq" element={<FAQ />} />                 {/* What to Expect */}
-          <Route path="/contact" element={<Contact />} />         {/* Get in Touch */}
+          <Route path="/contact2" element={<Contact />} />         {/* Get in Touch */}
           <Route path="/book" element={<Book />} />               {/* Start Your Lark */}
 
           {/* Optional sub-routes */}
--- src/pages/contact2.tsx
+++
@@ -1,7 +0,0 @@
-export default function Contact() {
-    return (
-      <div className="p-10 text-2xl font-elk text-center">
-        The ELK Story
-      </div>
-    );
-  }
