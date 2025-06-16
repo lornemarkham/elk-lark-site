c12531b1fc20ac35655a839c14e4f52cfeb89711
lorne markham <lornemarkham@lornes-MacBook-Pro.local>
2025-06-16 10:33:51 -0700

    test

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,13 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 
 import Header from "./components/Header";
-
 import Home from "./pages/Home";
-import About from "./pages/about2";
-import Services from "./pages/services2";
-import Property from "./pages/property2";
+import About from "./pages/About";
+import Services from "./pages/Services";
+import Property from "./pages/Property";
 import Gallery from "./pages/Gallery";
 import FAQ from "./pages/Faq";
-import Contact from "./pages/contact2";
+import Contact from "./pages/Contact";
 import Book from "./pages/Book";
 
 // Optional sub-pages if you're using them
@@ -30,7 +29,7 @@ export default function App() {
           <Route path="/property" element={<Property />} />       {/* Basecamp */}
           <Route path="/gallery" element={<Gallery />} />         {/* The Lark Life */}
           <Route path="/faq" element={<FAQ />} />                 {/* What to Expect */}
-          <Route path="/contact2" element={<Contact />} />         {/* Get in Touch */}
+          <Route path="/contact" element={<Contact />} />         {/* Get in Touch */}
           <Route path="/book" element={<Book />} />               {/* Start Your Lark */}
 
           {/* Optional sub-routes */}
@@ -43,3 +42,10 @@ export default function App() {
     </>
   );
 }
+
+
+
+
+
+git mv src/pages/Property.tsx src/pages/Property_temp.tsx
+git mv src/pages/Property_temp.tsx src/pages/Property.tsx
