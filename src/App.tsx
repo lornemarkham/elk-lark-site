cb2d0310e6eece3f09c7cc4e640268d338ff2b00
lorne markham <lornemarkham@lornes-MacBook-Pro.local>
2025-06-16 03:41:39 -0700

    test

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 
 import Home from "./pages/Home";
-import About from "./pages/About";
-import Services from "./pages/Services";
+import About from "./pages/about2";
+import Services from "./pages/services2";
 import Property from "./pages/Property";
 import Gallery from "./pages/Gallery";
 import FAQ from "./pages/Faq";
