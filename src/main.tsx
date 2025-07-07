2825c1a6db7cdb8b07e9c3951c21e4fcd719edca
Lorne Markham <lorne.markham@gmail.com>
2025-07-06 18:02:10 -0700

    more fixes

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,6 @@ import App from "./App";
 import "./index.css";
 import { Analytics } from "@vercel/analytics/next"
 
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
