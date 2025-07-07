b6b6e723d8517d0fa9abe74bbd5b54e010fcc56b
Lorne Markham <lorne.markham@gmail.com>
2025-07-06 23:59:38 -0700

    analytics

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 import { Analytics } from "@vercel/analytics/next"
+// test
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
