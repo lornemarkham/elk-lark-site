f2af376d71aca8311513b076c77677ecc7a1d054
Lorne Markham <lorne.markham@gmail.com>
2025-07-07 00:23:34 -0700

    Add Vercel Analytics to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
+import { Analytics } from "@vercel/analytics/react";  // ✅ use the new react import
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
+      <Analytics /> {/* ✅ this tracks all page views */}
     </BrowserRouter>
   </React.StrictMode>
 );
